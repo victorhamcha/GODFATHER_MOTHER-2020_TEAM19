dd4f1dd585e694e6277a3edcb0965711e3af99a7
Leo <Aqu4men@gmail.com>
2020-09-02 00:25:13 +0200

    Code des dialogues

---
+++ GODFATHERMOTHER2020/GODFATHER2020/Assets/Scripts/Léo/Dialogue/DialogueBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    public class DialogueBase : MonoBehaviour
+    {
+        public bool finished { get; private set; }
+       protected IEnumerator WriteText(string input , Text tectholder, float delay, float delayBetween)
+        {
+            for(int i = 0; i < input.Length; i++)
+            {
+                tectholder.text += input[i];
+                yield return new WaitForSeconds(delay);
+            }
+            yield return new WaitForSeconds(delayBetween);
+            finished = true;
+        }
+    }
+}
---
+++ GODFATHERMOTHER2020/GODFATHER2020/Assets/Scripts/Léo/Dialogue/DialogueLine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{   public class DialogueLine : DialogueBase
+    {
+        private Text textHolder;
+        [SerializeField] private string input;
+        [SerializeField] private float delay;
+        [SerializeField] private float delayBetween;
+        private void Awake()
+        {
+            textHolder = GetComponent<Text>();
+
+           
+        }
+        private void Start()
+        {
+            StartCoroutine(WriteText(input, textHolder, delay,delayBetween));
+        }
+    }
+  
+}
+
