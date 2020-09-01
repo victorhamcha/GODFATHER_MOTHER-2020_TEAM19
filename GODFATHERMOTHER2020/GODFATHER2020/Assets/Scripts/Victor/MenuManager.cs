af2488b7a494220a2f2513a035bc6ea219309609
Hamcha <victor.hamcha@gmail.com>
2020-09-01 16:49:06 +0200

    Menu changement

--- GODFATHERMOTHER2020/GODFATHER2020/Assets/Scripts/Victor/MenuManager.cs
+++ GODFATHERMOTHER2020/GODFATHER2020/Assets/Scripts/Victor/MenuManager.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BLANC : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+   
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         
     }
+
+
+
+
 }
