0c6fe4bd11fe727e7b58db39d159f60886078f49
Nolan Jutard <55735024+JutardN@users.noreply.github.com>
2020-09-01 17:39:10 +0200

    Movement player(left/right/jump) to polish

---
+++ GODFATHERMOTHER2020/GODFATHER2020/Assets/Scripts/Nolan/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool canJump;
+    public bool ground=true;
+    private float jump = 10f;
+    public float speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") < -0.3)
+        {
+            transform.Translate(-speed, 0, 0);
+            //mySpriteRenderer.flipX = true;
+        }
+        else if (Input.GetAxis("Horizontal") > 0.3)
+        {
+            transform.Translate(speed, 0, 0);
+            //mySpriteRenderer.flipX = false;
+        }
+
+        if (Input.GetKeyDown("joystick button 1") && ground)
+        {
+            canJump = true;
+            //myAnimator.SetBool("isJumping", true);
+            ground = false;
+            Debug.Log("Jump");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!ground)
+        {
+            canJump = false;
+            transform.Translate(Vector3.up * jump * Time.deltaTime, Space.World);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            ground = true;
+            Debug.Log("ok");
+            //myAnimator.SetBool("isJumping", false);
+        }
+    }
+}
