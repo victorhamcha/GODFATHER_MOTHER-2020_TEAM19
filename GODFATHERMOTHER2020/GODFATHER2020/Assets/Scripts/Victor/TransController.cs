978da6d47c65d3a6174ebd511aada1ec8d29588f
Hamcha <victor.hamcha@gmail.com>
2020-09-02 11:32:07 +0200

    Changement pour diva

---
+++ GODFATHERMOTHER2020/GODFATHER2020/Assets/Scripts/Victor/TransController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransController : MonoBehaviour
+{
+   [Header("Mouvement")]
+    public bool ground = true;
+    public float jumpForce = 10f;
+    public float speed = 0.1f;
+    [Header("Mele Attack")]
+    public float couldownMeleAttack = 0.3f;
+    private float timeMeleAttack = 0;
+    private bool isAttacking;
+    //pour trans
+    public GameObject hairCollider;
+    
+    [Header("Bullet Attack")]
+    public float couldownSound = 0.1f;
+    private float timerSound = 0;
+    public GameObject bulletSound;
+    [Header("Health")]
+    public float health = 100f;
+    private bool block=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") < -0.3)
+        {
+            transform.Translate(-speed, 0, 0);
+            //mySpriteRenderer.flipX = true;
+        }
+        else if (Input.GetAxis("Horizontal") > 0.3)
+        {
+            transform.Translate(speed, 0, 0);
+            //mySpriteRenderer.flipX = false;
+        }
+
+        if (!ground)
+        {
+            transform.Translate(Vector3.up * jumpForce * Time.deltaTime, Space.World);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space) && ground)
+        {
+            ground = false;
+            Debug.Log("Jump");
+        }
+
+
+        //Attack Mele
+        if (Input.GetKey(KeyCode.B) && !isAttacking)
+        {
+            isAttacking = true;
+            //pour trans
+            hairCollider.SetActive(true);
+        }
+        else if(isAttacking&&timeMeleAttack>0)
+        {
+            timeMeleAttack -= Time.deltaTime;
+        }
+        else if(isAttacking && timeMeleAttack <= 0)
+        {
+            timeMeleAttack = couldownMeleAttack;
+            isAttacking = false;
+            //pour trans
+            hairCollider.SetActive(false);
+
+        }
+      
+
+        //Attack Bullet
+        if (Input.GetKey(KeyCode.A)&&timerSound<=0)
+        {
+            timerSound = couldownSound;
+        }
+        else if(timerSound>0)
+        {
+            timerSound -= Time.deltaTime;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            ground = true;
+           
+            //myAnimator.SetBool("isJumping", false);
+        }
+    }
+}
