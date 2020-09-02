12e4094d4bd6aac6e71681a1c3c12094ddc9786e
Nolan Jutard <55735024+JutardN@users.noreply.github.com>
2020-09-02 12:03:57 +0200

    Adding boss script

---
+++ GODFATHERMOTHER2020/GODFATHER2020/Assets/Scripts/Nolan/BossManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossManager : MonoBehaviour
+{
+    [Header("Stats")]
+    public int hp;
+    public int dps;
+    protected enum State { Melee, Distance};
+    protected State etat;
+
+    [Header("Distance")]
+    public Transform[] firePoint;
+    public GameObject bullet;
+    private int rand;
+    private float cooldownBullet = 0.0f;
+    private bool shoot;
+
+
+
+    [Header("Melee")]
+    public Transform target;
+    private float cooldown=0.0f;
+    private bool attacking;
+    private bool canHit;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            Die();
+        }
+        if (canHit)
+        {
+            etat = State.Melee;
+        }
+        else
+        {
+            etat = State.Distance;
+        }
+        Attack();
+
+        if (attacking)
+        {
+            cooldown -= Time.deltaTime;
+            if (cooldown <= 0)
+            {
+                attacking = false;
+            }
+        }
+        if (shoot)
+        {
+            cooldownBullet -= Time.deltaTime;
+            if (cooldownBullet <= 0)
+            {
+                shoot = false;
+            }
+        }
+    }
+
+    public void Attack()
+    {
+        if (etat == State.Melee)
+        {
+            if (!attacking)
+            {
+                target.gameObject.GetComponent<TransController>().health -= dps;
+                cooldown = 5.0f;
+                attacking = true;
+            }
+        }
+        else if(!shoot)
+        {
+            shoot = true;
+            cooldownBullet = 3.0f;
+            rand=Random.Range(0, firePoint.Length);
+            GameObject bulletGameObject = (GameObject)Instantiate(bullet, firePoint[rand].position, firePoint[rand].rotation);
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            canHit = true;
+        } 
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            canHit = false;
+        }
+    }
+}
---
+++ GODFATHERMOTHER2020/GODFATHER2020/Assets/Scripts/Nolan/BulletBoss.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBoss : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 2;
+    private int dps=5;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position += -transform.right * speed * Time.fixedDeltaTime;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<TransController>().health -= dps;
+            Destroy(this.gameObject);
+        }
+    }
+}
