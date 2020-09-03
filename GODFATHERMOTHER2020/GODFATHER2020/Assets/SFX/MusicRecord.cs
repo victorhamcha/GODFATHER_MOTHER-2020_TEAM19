103b2719fb0f93c5141efa1d26f427d626919522
Kevin-RA <kevin.ribeiro-ansoult@edu.devinci.fr>
2020-09-04 01:09:41 +0200

    Add up drive + MusicManag
    Add up of the SFX
    Add up of a MusicManager
    Add up of a Option Scene
    Add of a Slider for the Volume

---
+++ GODFATHERMOTHER2020/GODFATHER2020/Assets/SFX/MusicRecord.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+public enum MusicType
+{
+    Menu, Game, Victory, Credits, GameOver
+}
+
+public class MusicRecord : MonoBehaviour
+{
+    public SceneMusic[] musicForScenes;
+    private AudioClip currentLoop;
+    
+
+    public AudioSource audioSource;
+    
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Audio");
+        if(objs.Length >1){
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        StartSceneMusic();
+        SceneManager.sceneLoaded += (scene, mode) => StartSceneMusic();
+    }
+
+    private void StartSceneMusic()
+    {
+        var sceneInfo = GameObject.FindWithTag("SceneInfo")?.GetComponent<SceneInfo>();
+        var currentSceneType = (sceneInfo != null) ? sceneInfo.sceneMusicType : MusicType.Game;
+        var currentMusicInfo = musicForScenes.First(music => music.musicType == currentSceneType);
+        currentLoop = currentMusicInfo.loop;
+        audioSource.clip = currentMusicInfo.intro;
+        audioSource.loop = false;
+        audioSource.Play();
+    }
+
+    private void Update()
+    {
+        if (audioSource.isPlaying) return;
+        audioSource.clip = currentLoop;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+}
+
+[Serializable]
+public class SceneMusic
+{
+    public MusicType musicType;
+    public AudioClip intro;
+    public AudioClip loop;
+}
---
+++ GODFATHERMOTHER2020/GODFATHER2020/Assets/SFX/SliderMusicScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderMusicScript : MonoBehaviour
+{
+
+    public float Value;
+
+    private Slider zlider;
+
+    public GameObject AS;
+
+    private void Awake()
+    {
+        zlider = this.GetComponent<Slider>();
+        AS = GameObject.Find("MusicManager");
+        zlider.value = AS.GetComponent<AudioSource>().volume;
+    }
+    void Update()
+    {
+        Value = zlider.value;
+        AS.GetComponent<AudioSource>().volume = zlider.value;
+    }
+}
